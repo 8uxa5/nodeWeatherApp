ec1376f699bc06f9def55b8c9c40fc36b0a2c34a
Darius Maciunas <dmaciunas@outlook.com>
2019-11-26 16:56:51 +0000

    removed unnecessary file

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Hello from ClientSIDE JavaScript FILE in public dir!!!");
-
 const weatherForm = document.querySelector("form");
 const searchTerm = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
